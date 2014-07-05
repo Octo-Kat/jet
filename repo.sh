8cb7288a2f7444959b732c6c1dce3333666dd46a
Donald Hoskins <grommish@gmail.com>
2014-07-04 22:52:28 -0400

    Update to repo system

--- repo.sh
+++ repo.sh
@@ -11,18 +11,45 @@
 ## jet/repo.sh sam, jet/repo.sh nex, etc
 ##
 ## Defaults to master branch (all devices) unless specified.
-
+CWD=$(pwd)
 DEVICE_TREE=$1
-: ${DEVICE_TREE:="master"}
+MANI_REPO="vendor/oct/prebuilt/common/manifests"
+VENDOR_REPO="vendor/oct"
 
-cd ".repo/local_manifests"
-git checkout $DEVICE_TREE
-git pull origin $DEVICE_TREE
+cd $VENDOR_REPO
+echo "Updating Repo"
+git fetch
+git pull
+echo "Done Updating"
+cd $CWD
 
-cd "../.."
-RDATE=`date +"%m-%d-%y %T"`
-echo $RDATE >> Repo_ChangeLog
-#repo sync 2>tmp &&  egrep '(From)|(->)' tmp >> Repo_ChangeLog
+if [[ $DEVICE_TREE = "" ]]
+  then
+  echo "Pick a repo:"
+  for entry in "$MANI_REPO"/*
+  do
+    repname=$(basename $entry)
+    if [[ $repname = "cm.xml" || $repname = "oct.xml" ]]
+    then
+      continue
+    fi
+    echo "${repname%.xml}"
+  done
+  exit
+fi
+
+cd $CWD
+echo "CWD : $CWD"
+if [ ! -d ".repo/local_manifests" ]; then
+  mkdir -p ".repo/local_manifests"
+fi
+touch ".repo/local_manifests/OctOs.xml"
+cat "$MANI_REPO/oct.xml" > ".repo/local_manifests/OctOs.xml"
+cat "$MANI_REPO/cm.xml" >> ".repo/local_manifests/OctOs.xml"
+for DEVICE_TREE in "$@"
+do
+cat "$MANI_REPO/$DEVICE_TREE.xml" >> ".repo/local_manifests/OctOs.xml"
+done
+echo "</manifest>" >> ".repo/local_manifests/OctOs.xml"
 
 repo sync
-
